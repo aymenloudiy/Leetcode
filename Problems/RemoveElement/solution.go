e1d510ac402fd6b7849682006e2ff3613064e0d1
Nimuel <yiduolnemya@gmail.com>
2025-08-03 14:57:33 -0400

    Remove unused function error

--- Problems/RemoveElement/solution.go
+++ Problems/RemoveElement/solution.go
@@ -1,5 +1,5 @@
 package removeelement
-func removeElement(nums []int, val int) int {
+func _(nums []int, val int) int {
     k := 0
     for index,element := range nums{
         if element != val {
